110dd88b551dd7a784721e84b87c80f1e07abaa3
Neel <deb.neelsayar@gmail.com>
2021-06-23 14:17:57 +0530

    added spinner while axios call is made

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -10,3 +10,5 @@ export const Spinner = () => (
     />
   </Fragment>
 );
+
+export default Spinner;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 
 const User = ({ users, loading }) => {
-  return (
-    <div style={userStyle}>
-      {!loading && users.map((user) => <UserItem key={user.id} user={user} />)}
-    </div>
-  );
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
 };
 
 const userStyle = {
